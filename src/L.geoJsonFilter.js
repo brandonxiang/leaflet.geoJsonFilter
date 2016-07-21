d1c54e87ae6d4041d6bcd5adbaa1c97e848bdb66
brandonxiang <1542453460@qq.com>
2016-07-21 10:36:22 +0800

    update version 0.1.0

--- src/L.geoJsonFilter.js
+++ src/L.geoJsonFilter.js
@@ -1,19 +1,3 @@
-// L.GeoJSON.extend({
-//     initialize: function(geojson, options) {
-//         this._geojson = geojson
-//         L.GeoJSON.prototype.initialize.call(this, geojson, options)
-//     },
-//     setFilter: function(_) {
-//         this.options.filter = _
-//         if (this._geojson) {
-//             this.clearLayers()
-//             this.addData(this._geojson)
-//         }
-//         return this
-//     }
-// })
-//
-
 L.GeoJSON.include({
     setFilter: function(_) {
         if (!this._geojson) {
